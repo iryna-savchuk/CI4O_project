a6a57ee55813103870645bc535bd62568f8e6a20
Iryna S <iryna.o.savchuk@gmail.com>
2023-05-22 03:33:28 +0100

    Adjusted representation of Individual, changed calculation of Fitness, separated data loading

--- charles/charles.py
+++ charles/charles.py
@@ -1,24 +1,30 @@
 #from random import shuffle, choice, sample
-from random import random  # random() - Returns a random float number between 0 and 1
+import random
 from operator import attrgetter
 from copy import deepcopy
+from tqdm import tqdm
 
-import numpy as np
+random.seed(42)
 
 class Individual:
     def __init__(
-        self,
-        representation=None,
-        input=28*28,
-        hidden=512,
-        output=10
+            self,
+            representation=None,
+            input=None,
+            hidden=None,
+            output=None,
+            valid_range=[]
     ):
         if representation is None:
-            weights_1 = np.random.rand(input * hidden).reshape(input, hidden)
-            biases_1 = np.random.rand(hidden).reshape(hidden, )
-            weights_2 = np.random.rand(hidden * output).reshape(hidden, output)
-            biases_2 = np.random.rand(output).reshape(output, )
-            self.representation = [weights_1, biases_1, weights_2, biases_2]
+            size = input * hidden + hidden + hidden * output + output
+            #random_weights = np.random.uniform(valid_range[0], valid_range[1], size=size)
+            random_weights = [random.uniform(valid_range[0], valid_range[1]) for _ in range(size)]
+            self.representation = {
+                "weights": random_weights,
+                "input": input,
+                "hidden": hidden,
+                "output": output
+            }
         else:
             self.representation = representation
         self.fitness = self.get_fitness()
@@ -30,37 +36,41 @@ class Individual:
         raise Exception("You need to monkey patch the neighbourhood function.")
 
     def index(self, value):
-        return self.representation.index(value)
+        return self.representation["weights"].index(value)
 
     def __len__(self):
-        return len(self.representation)
+        return len(self.representation["weights"])
 
     def __getitem__(self, position):
-        return self.representation[position]
+        return self.representation["weights"][position]
 
     def __setitem__(self, position, value):
-        self.representation[position] = value
+        self.representation["weights"][position] = value
 
     def __repr__(self):
-        weights_print = ""
-        for weights in self.representation:
-            weights_print = weights_print+" "+str(weights.shape)
-        return f"Individual: {weights_print}; Fitness: {self.fitness}\n"
-
-
+        #return f"Individual: {self.representation};" #Fitness: {self.fitness}\n"
+        #return '\nIndividual:'+'\n'.join(f"{k}: {v}" for k, v in self.representation.items() if k!="weights")+'\nFitness: '+str(self.fitness)+'\n'
+        representation_print = str(self.representation["input"])+", " + str(self.representation["hidden"])+", " + str(self.representation["output"])
+        return f"\nIndividual: {representation_print}; Fitness: {self.fitness}"
 class Population:
     def __init__(self, size, optim, **kwargs):
+        print("Generating initial population...")
         self.individuals = []
         self.size = size
         self.optim = optim
-        for _ in range(size):
+        for _ in tqdm(range(size)):
             self.individuals.append(
-                Individual()
+                Individual(
+                    input=kwargs["sol_input"],
+                    hidden=kwargs["sol_hidden"],
+                    output=kwargs["sol_output"],
+                    valid_range=kwargs["valid_range"],
+                )
             )
 
     def evolve(self, gens, xo_prob, mut_prob, select, mutate, crossover, elitism):
 
-        for i in range(gens):
+        for i in tqdm(range(gens)):
             new_pop = []
 
             if elitism:
@@ -72,19 +82,29 @@ class Population:
             while len(new_pop) < self.size:
                 parent1, parent2 = select(self), select(self)
 
-                if random() < xo_prob:
+                if random.random() < xo_prob:
                     offspring1, offspring2 = crossover(parent1, parent2)
                 else:
                     offspring1, offspring2 = parent1, parent2
 
-                if random() < mut_prob:
+                if random.random() < mut_prob:
                     offspring1 = mutate(offspring1)
-                if random() < mut_prob:
+                if random.random() < mut_prob:
                     offspring2 = mutate(offspring2)
 
-                new_pop.append(Individual(representation=offspring1))
+                new_pop.append(Individual(representation={
+                    "weights": offspring1,
+                    "input": parent1.representation["input"],
+                    "hidden": parent1.representation["hidden"],
+                    "output": parent1.representation["output"],
+                }))
                 if len(new_pop) < self.size:
-                    new_pop.append(Individual(representation=offspring2))
+                    new_pop.append(Individual(representation={
+                        "weights": offspring2,
+                        "input": parent1.representation["input"],
+                        "hidden": parent1.representation["hidden"],
+                        "output": parent1.representation["output"],
+                    }))
 
             if elitism:
                 if self.optim == "max":
@@ -101,6 +121,7 @@ class Population:
 
             self.individuals = new_pop
 
+            print("\nGeneration >>> ", i+1)
             if self.optim == "max":
                 print(f'Best Individual: {max(self, key=attrgetter("fitness"))}')
             elif self.optim == "min":
--- mnist.py
+++ mnist.py
@@ -1,50 +1,59 @@
 from charles.charles import Population, Individual
-from data.tsp_data import distance_matrix
+from data.mnist_data import train_images, train_labels
 from copy import deepcopy
-from charles.crossover import cycle_xo, pmx
+from charles.crossover import cycle_xo, pmx, single_point_co, arithmetic_xo
 from charles.mutation import swap_mutation, inversion_mutation
 from charles.selection import tournament_sel, fps
+
+import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.datasets import mnist
 
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-# Reshape and normalize the input images
-train_images = train_images.reshape(-1, 784) / 255.0
-test_images = test_images.reshape(-1, 784) / 255.0
 
 def get_fitness(self):
+    input = self.representation["input"]
+    hidden = self.representation["hidden"]
+    output = self.representation["output"]
+
     # Defining the architecture of a neural network
     nn = keras.Sequential([
-        layers.Dense(512, activation="relu", input_shape=(784,)),
-        layers.Dense(10, activation="softmax")
+        layers.Dense(hidden, activation="relu", input_shape=(input,)),
+        layers.Dense(output, activation="softmax")
     ])
+    current_weights = np.array(self.representation["weights"])
+    weights_1 = current_weights[:input * hidden].reshape(input, hidden)
+    biases_1 = current_weights[input * hidden:input * hidden + hidden].reshape(hidden, )
+    weights_2 = current_weights[input * hidden + hidden:input * hidden + hidden + hidden * output].reshape(hidden, output)
+    biases_2 = current_weights[input * hidden + hidden + hidden * output:].reshape(output, )
 
     # Setting the weights of the model
-    nn.set_weights(self.representation)
+    nn.set_weights([weights_1, biases_1, weights_2, biases_2])
 
     # Compiling the model
-    nn.compile(optimizer="rmsprop",
-               loss="sparse_categorical_crossentropy",
-               metrics=["accuracy"])
-    fitness = 1
-    test_loss, test_acc = nn.evaluate(train_images, train_labels)
-    return test_loss
+    nn.compile(metrics=["accuracy"])
+
+    _, train_acc = nn.evaluate(train_images, train_labels, verbose=0)
+    return train_acc
 
 
 # Monkey patching
 Individual.get_fitness = get_fitness
 
-
 pop = Population(
-    size=2,
-    optim="min")
-
+    size=30,
+    sol_input=28*28,
+    sol_hidden=512,
+    sol_output=10,
+    valid_range=[-1, 1],
+    optim="max")
 
 print(pop.individuals)
 #print(pop.individuals[0].representation)
 
-#pop.evolve(gens=5, select=tournament_sel, mutate=inversion_mutation, crossover=pmx,
-#           mut_prob=0.05, xo_prob=0.9, elitism=True)
+
+pop.evolve(gens=20,
+           select=tournament_sel, mutate=inversion_mutation, crossover=arithmetic_xo,
+           mut_prob=0.3, xo_prob=0.9,
+           elitism=True)
 
 
